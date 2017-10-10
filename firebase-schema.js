ed6e2ae0f09515b8bb39bc86bda7856ace262e4e
Razwan Rashidi <razwan.izmi@gmail.com>
2017-10-11 07:28:24 +0800

    Initial commit with Redux blueprint

---
+++ firebase-schema.js
@@ -0,0 +1,57 @@
+No perfect
+  - tradeoffs
+Keep shallow
+Repeat data is OK
+
+Home
+  All ducks
+
+Profile
+  User info
+  Users ducks
+
+Replies
+  Specific duck
+  Duck replies
+
+/users
+  uid
+    name
+    uid
+    avatar
+
+/ducks
+  duckId
+    avatar
+    duckId
+    name
+    text
+    timestamp
+    uid
+
+/likeCount
+  duckId
+
+/usersDuck
+  uid
+    duckId
+      avatar
+      duckId
+      name
+      text
+      timestamp
+      uid
+
+/replies
+  duckId
+    replyId
+      name
+      comment
+      uid
+      timestamp
+      avatar
+
+/userLikes
+  uid
+    duckId
+
