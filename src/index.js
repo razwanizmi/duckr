ead5f6ebbdd8958d3428afa2ffb8267024f59bfe
Razwan Rashidi <razwan.izmi@gmail.com>
2017-10-17 08:17:41 +0800

    Add redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import getRoutes from "./config/routes";
 import restricted from "helpers/restricted";
 import users from "redux/modules/users";
 
-const store = createStore(users, applyMiddleware(thunk));
+const store = createStore(
+  users,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 
 const checkAuth = component => {
   return restricted(component, store);
